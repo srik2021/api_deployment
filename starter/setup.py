8bd5ab7be13caacccef66a77bc04c598f1419eeb
srik2021 <srikanth.kotha@gmail.com>
2023-03-11 17:42:50 -0600

    Try to fix Github Actions pytest failure

--- starter/setup.py
+++ starter/setup.py
@@ -1,8 +1,10 @@
 import setuptools
 
 setuptools.setup(
-    name="starter",
-    version="0.0.0",
-    description="Starter code.",
-    author="Student",
+    name="salary_prediction",
+    version="0.1.0",
+    packages= ['src', 'tests'],
+    include_package_data=True,
+    description="Modeling code to train/test salary prediction model.  Also includes API to make predictions.",
+    author="Srikanth Kotha",
 )
