b685a79d94d7a2b664a645ce36414028c60e750c
srik2021 <srikanth.kotha@gmail.com>
2023-03-11 18:17:35 -0600

    change how packages are found

--- starter/setup.py
+++ starter/setup.py
@@ -3,11 +3,10 @@ import setuptools
 setuptools.setup(
     name="salary_prediction",
     version="0.1.0",
-    packages=['src', 'tests'],
+    packages=setuptools.find_packages(),
     include_package_data=True,
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
-
     description="Modeling code to train/test salary prediction model.  Also includes API to make predictions.",
     author="Srikanth Kotha",
 )
