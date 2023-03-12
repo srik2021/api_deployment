b0e7378e9230e27e5d77067e6afa4c04ff57af77
srik2021 <srikanth.kotha@gmail.com>
2023-03-11 18:09:47 -0600

    remove space from setup.py

--- starter/setup.py
+++ starter/setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="salary_prediction",
     version="0.1.0",
-    packages= ['src', 'tests'],
+    packages=['src', 'tests'],
     include_package_data=True,
     description="Modeling code to train/test salary prediction model.  Also includes API to make predictions.",
     author="Srikanth Kotha",
